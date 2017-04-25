1408c6e0ba68064b7621ae934d91e6760c204abe
Aaron Goodman <Amgoodman5@Aarons-MacBook-Air.local>
2017-04-25 09:08:46 -0600

    updates

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import {SearchComponent} from './search/search.component';
 import {AboutComponent} from './about/about.component';
 import {ArtistComponent} from './artist/artist.component';
 const appRoutes: Routes = [
-  {
+  { 
     path:'',
     component: SearchComponent
   },
